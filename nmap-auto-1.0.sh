6d739b4f76bf18087ef790d178bbe6d0a302d511
Wojtek <wojtekg4455@gmail.com>
2020-10-17 19:29:26 +0200

    Komentarz
    Zmiana echo + drobne literówki

--- nmap-auto-1.0.sh
+++ nmap-auto-1.0.sh
@@ -3,21 +3,21 @@
 # Skrypt skanujący z użyciem Nmap
 # By Wojtek 2020
 
-echo "\e[32m========================================================\e[0m"
-echo "\e[32m  _   _                          ____      _   _       \e[0m"       
-echo "\e[32m | \ | |                        / __ \    | | (_)      \e[0m"      
-echo "\e[32m |  \| |_ __ ___   __ _ _ __   | |  | |___| |_ _ _ __  \e[0m"  
-echo "\e[32m | | | | |_ | _ \ / _| | |_ \  | |  | / __| __| | |_ \ \e[0m"
-echo "\e[32m | |\  | | | | | | (_| | |_) | | |__| \__ \ |_| | | | |\e[0m"
-echo "\e[32m |_| \_|_| |_| |_|\__|_| |__/   \____/|___/\__|_|_| |_|\e[0m"
-echo "\e[32m                       | |                             \e[0m"
-echo "\e[32m                       |_|                             \e[0m"
-echo "\e[32m========================================================\e[0m"
+echo -e "\e[32m========================================================\e[0m"
+echo -e "\e[32m  _   _                          ____      _   _       \e[0m"       
+echo -e "\e[32m | \ | |                        / __ \    | | (_)      \e[0m"      
+echo -e "\e[32m |  \| |_ __ ___   __ _ _ __   | |  | |___| |_ _ _ __  \e[0m"  
+echo -e "\e[32m | | | | |_ | _ \ / _| | |_ \  | |  | / __| __| | |_ \ \e[0m"
+echo -e "\e[32m | |\  | | | | | | (_| | |_) | | |__| \__ \ |_| | | | |\e[0m"
+echo -e "\e[32m |_| \_|_| |_| |_|\__|_| |__/   \____/|___/\__|_|_| |_|\e[0m"
+echo -e "\e[32m                       | |                             \e[0m"
+echo -e "\e[32m                       |_|                             \e[0m"
+echo -e "\e[32m========================================================\e[0m"
 
-echo "\e[32m Podaj adres IPv4 :\e[0m"
+echo -e "\e[32m Podaj adres IPv4 :\e[0m"
 read ADRES_IP
 
-echo "\e[32m Jakie skanowanie przeprowadzić ? :\e[0m"
+echo -e "\e[32m Jakie skanowanie przeprowadzić ? :\e[0m"
 select WYBOR in SZYBKIE GLEBOKIE
 do
 	case $WYBOR in
@@ -31,5 +31,5 @@ do
 		;;
 	*) echo "Brak wyboru !"
 	esac
-	brake
+	break
 done
