022d38d1b027446d08c4e789c589795d3fa2814d
Wojtek <wojtekg4455@gmail.com>
2020-10-27 23:22:38 +0100

    Komenatarz
    Dodano wykrywanie adresu IPv4 , dodano wykrywanie domeny, usprawniono
    opisy i pewne funkcjonalności związane z warunkami if dla skanowania
    głębokiego i szybkiego.

--- nmap-auto-1.0.sh
+++ nmap-auto-1.0.sh
@@ -47,7 +47,10 @@ clear
 # Definicja zmiennych używanych w skrypcie
 nmap_h="nmap -sn"
 nmap_s="nmap"
-nmap_g="nmap -sS -sU -v -O"
+nmap_g="nmap -sS -sU -v -O -p 0-"
+domena_vaild="^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$"
+IP_VAILD="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$" 
+ZAKRES_PORTY="^[0-9]+$"
 wynik_s="wynik.txt"
 data="`date`"
 IP="`ip a | grep 'state UP' -A2 | tail -n1 | awk -F'[/ ]+' '{print $3}'`"
@@ -123,9 +126,10 @@ do {
 				echo -e "\e[32mTwoje obecne IP : $IP\e[0m"	
 				echo -e "\e[32mPodaj adres IPv4 np. 127.0.0.1 lub domenę np. nmap.org :\e[0m"
 				read ADRES_IP
-				# Wykrywająca adres IP (wmiarę poprawnego)	
-				if [[ $ADRES_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then {
-						echo -e "\e[32mRozpoczynam skanowanie - szybkie dla adresu/domeny : $ADRES_IP\e[0m"	
+				# Wykrywanie adres IP (wmiarę poprawnego)	
+				if [[ $ADRES_IP =~ $IP_VAILD ]]; 
+				then {
+						echo -e "\e[32mRozpoczynam skanowanie - szybkie dla adresu : $ADRES_IP\e[0m"	
 						if (($EUID)); then {	
 						echo -e "\e[32mWykryłem, że nie pracujesz jako root włączę nmap poprzez polecenie sudo ! może być wymagane podanie hasła :\e[0m"
 						echo "Data skanowania: $data" > $wynik_s
@@ -142,44 +146,99 @@ do {
 						echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
 						wynik;	
 						} 
-						fi 
-						}
-						else {
-						echo -e "\e[33mPodano błędny adres IP !\e[0m"
-						}
-				
-				fi
-				;;
-			"Skanowanie - głębokie")
-				echo -e "\e[32mPracujesz jako :\e[0m"; whoami 
-				echo -e "\e[32mTwoje obecne IP : $IP\e[0m"
-				echo -e "\e[32mPodaj adres IPv4 np. 127.0.0.1 lub domenę np. nmap.org :\e[0m"
-				read ADRES_IP	
-				# Wykrywająca adres IP (wmiarę poprawnego)	
-				if [[ $ADRES_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then {
-						echo -e "\e[32mRozpoczynam skanowanie - głębokie dla adresu/domeny : $ADRES_IP\e[0m"	
+						fi
+				 	}	
+				elif [[ $ADRES_IP =~ $domena_vaild ]]; 
+				then {	
+						echo -e "\e[32mRozpoczynam skanowanie - szybkie dla domeny : $ADRES_IP\e[0m"	
 						if (($EUID)); then {	
 						echo -e "\e[32mWykryłem, że nie pracujesz jako root włączę nmap poprzez polecenie sudo ! może być wymagane podanie hasła :\e[0m"
 						echo "Data skanowania: $data" > $wynik_s
 						echo "Skanowałeś następujący cel : $ADRES_IP" >> $wynik_s 	
-						sudo $nmap_g $ADRES_IP >> $wynik_s
+						sudo $nmap_s $ADRES_IP >> $wynik_s
 						cat $wynik_s
 						echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
 						wynik;
 						} else {
 						echo "Data skanowania: $data" > $wynik_s
 						echo "Skanowałeś następujący cel : $ADRES_IP" >> $wynik_s 
-						$nmap_g $ADRES_IP >> $wynik_s
+						$nmap_s $ADRES_IP >> $wynik_s
 						cat $wynik_s
 						echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
 						wynik;	
 						} 
-						fi 
+						fi
+					}
+				else {
+					echo -e "\e[33mPodano błędny adres IP lub domenę !\e[0m"
+				}
+				fi
+			;;
+			"Skanowanie - głębokie")
+				echo -e "\e[32mPracujesz jako :\e[0m"; whoami 
+				echo -e "\e[32mTwoje obecne IP : $IP\e[0m"
+				echo -e "\e[32mPodaj adres IPv4 np. 127.0.0.1 lub domenę np. nmap.org :\e[0m"
+				read ADRES_IP	
+				# Wykrywająca adres IP (wmiarę poprawnego)	
+				if [[ $ADRES_IP =~ $IP_VAILD ]]; then {
+						read -p "Podaj porty do przeskanowania z zakresu od 0 do (Max to 65535)" PORTY
+						if (($PORTY <= 65535)); then {
+							echo -e "\e[32mRozpoczynam skanowanie - głębokie dla adresu : $ADRES_IP\e[0m"	
+							if (($EUID)); then {	
+							echo -e "\e[32mWykryłem, że nie pracujesz jako root włączę nmap poprzez polecenie sudo ! może być wymagane podanie hasła :\e[0m"
+							echo "Data skanowania: $data" > $wynik_s
+							echo "Skanowałeś następujący cel : $ADRES_IP" >> $wynik_s 	
+							echo "skanuje poleceniem : $nmap_g$PORTY $ADRES_IP"
+							sudo $nmap_g$PORTY $ADRES_IP >> $wynik_s
+							cat $wynik_s
+							echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
+							wynik;
+							} else {
+							echo "Data skanowania: $data" > $wynik_s
+							echo "Skanowałeś następujący cel : $namp_g 0-$ADRES_IP" >> $wynik_s 
+							$nmap_g$PORTY $ADRES_IP >> $wynik_s
+							cat $wynik_s
+							echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
+							wynik;	
+							} 
+							fi 
 						}
 						else {	
-						echo -e "\e[33mPodano błędny adres IP !\e[0m"
+							echo -e "\e[33mPodaj porty poprawnie !\e[0m"
+						} fi
+					
+				}
+				else {
+					if [[ $ADRES_IP =~ $domena_vaild ]]; then {
+						read -p "Podaj porty do przeskanowania z zakresu od 0 do (Max to 65535)" PORTY
+						if (($PORTY <= 65535)); then {
+							echo -e "\e[32mRozpoczynam skanowanie - głębokie dla domeny : $ADRES_IP\e[0m"	
+							if (($EUID)); then {	
+							echo -e "\e[32mWykryłem, że nie pracujesz jako root włączę nmap poprzez polecenie sudo ! może być wymagane podanie hasła :\e[0m"
+							echo "Data skanowania: $data" > $wynik_s
+							echo "Skanowałeś następujący cel : $ADRES_IP" >> $wynik_s 	
+							echo "skanuje poleceniem : $nmap_g$PORTY $ADRES_IP"
+							sudo $nmap_g$PORTY $ADRES_IP >> $wynik_s
+							cat $wynik_s
+							echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
+							wynik;
+							} else {
+							echo "Data skanowania: $data" > $wynik_s
+							echo "Skanowałeś następujący cel : $namp_g 0-$ADRES_IP" >> $wynik_s 
+							$nmap_g$PORTY $ADRES_IP >> $wynik_s
+							cat $wynik_s
+							echo -e "\e[32mWynik skanowania zapisałem w pliku $wynik_s\e[0m"
+							wynik;	
+							} 
+							fi 
 						}
-				
+						else {	
+							echo -e "\e[33mPodaj porty poprawnie !\e[0m"
+						} fi
+					} else {
+					echo -e "\e[33mPodano błędny adres IP lub domenę !\e[0m"
+					} fi
+				} 
 				fi
 			;;			
 			"Skanowanie własnego IP")
@@ -221,7 +280,7 @@ do {
 			;;
 			"Wyjście")
 				clear
-				exit
+				exit 1
 			;;
 	*) echo "Brak wyboru !"
 	esac
@@ -229,5 +288,6 @@ do {
 done
 }
 echo -e "\e[32mBy Curar :) 2020 r.\e[0m"
+unset PORTY
 pauza
 done
